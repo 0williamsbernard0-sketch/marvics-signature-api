0293e5b8bab8b22418049df9d4a33f691785206c
0williamsbernard0-sketch <0williamsbernard0@gmail.com>
2026-07-10 02:22:47 +0100

    Create wallet-adapter.interface.ts

---
+++ src/modules/wallets/adapters/wallet-adapter.interface.ts
@@ -0,0 +1,31 @@
+export interface AddressResult {
+  address: string;
+  providerRef: string;
+}
+
+export interface VerifiedDepositEvent {
+  txHash: string;
+  chain: string;
+  asset: string;
+  amount: string;
+  confirmations: number;
+  rawPayload: unknown;
+}
+
+export interface WithdrawalParams {
+  asset: string;
+  amount: string;
+  destinationAddress: string;
+}
+
+export interface WithdrawalResult {
+  providerRef: string;
+  txHash?: string;
+  rawResponse: unknown;
+}
+
+export interface WalletAdapter {
+  createDepositAddress(userId: string, chain: string, asset: string): Promise<AddressResult>;
+  handleWebhook(rawPayload: unknown, signatureHeader: string): Promise<VerifiedDepositEvent>;
+  createWithdrawal(params: WithdrawalParams): Promise<WithdrawalResult>;
+}
