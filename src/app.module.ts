54a14f4e8660f148f3d56971fb7370134bc4a66e
0williamsbernard0-sketch <0williamsbernard0@gmail.com>
2026-07-10 16:37:46 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,6 @@ import { AuthModule } from './modules/auth/auth.module';
 import { LedgerModule } from './modules/ledger/ledger.module';
 import { WalletsModule } from './modules/wallets/wallets.module';
 import { validateEnv } from './common/config/env.validation';
-// If req.rawBody is undefined despite rawBody:true, add this in main.ts
-// right after app creation, before app.listen():
-import * as bodyParser from 'body-parser';
-
-app.use(
-  '/v1/webhooks/tatum',
-  bodyParser.raw({ type: 'application/json' }),
-);
 
 @Module({
   imports: [
