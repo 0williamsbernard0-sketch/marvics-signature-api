6450a3fcdb464727ff192c0e907582f5824fee23
0williamsbernard0-sketch <0williamsbernard0@gmail.com>
2026-07-10 14:32:23 +0100

    Update wallets.module.ts

--- src/modules/wallets/wallets.module.ts
+++ src/modules/wallets/wallets.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { LedgerModule } from '../ledger/ledger.module';   // ADD
 import { WalletsService } from './wallets.service';
 import { WalletsController } from './wallets.controller';
 import { TatumAdapter } from './adapters/tatum.adapter';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, LedgerModule],       // ADD LedgerModule
   controllers: [WalletsController],
   providers: [WalletsService, TatumAdapter],
   exports: [WalletsService],
