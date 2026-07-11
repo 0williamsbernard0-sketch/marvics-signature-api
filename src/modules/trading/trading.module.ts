451d96e0c9cfc2e62abbb3d72646f7769119d1f7
0williamsbernard0-sketch <0williamsbernard0@gmail.com>
2026-07-12 00:47:03 +0100

    Update trading.module.ts

--- src/modules/trading/trading.module.ts
+++ src/modules/trading/trading.module.ts
@@ -4,11 +4,17 @@ import { LedgerModule } from '../ledger/ledger.module';
 import { TradingService } from './trading.service';
 import { TradingController } from './trading.controller';
 import { BybitAdapter } from './adapters/bybit.adapter';
+import { BinanceAdapter } from './adapters/binance.adapter';
 
 @Module({
   imports: [PrismaModule, LedgerModule],
   controllers: [TradingController],
-  providers: [TradingService, BybitAdapter],
+  providers: [
+    TradingService,
+    BybitAdapter,
+    BinanceAdapter,
+    { provide: BybitAdapter, useExisting: BinanceAdapter },
+  ],
   exports: [TradingService],
 })
 export class TradingModule {}
