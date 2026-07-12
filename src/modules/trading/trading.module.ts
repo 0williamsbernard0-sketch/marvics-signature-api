d3221a10d4aef972d73c833e0dac69b252f29344
0williamsbernard0-sketch <0williamsbernard0@gmail.com>
2026-07-12 22:01:43 +0100

    Update trading.module.ts

--- src/modules/trading/trading.module.ts
+++ src/modules/trading/trading.module.ts
@@ -3,15 +3,12 @@ import { PrismaModule } from '../../prisma/prisma.module';
 import { LedgerModule } from '../ledger/ledger.module';
 import { TradingService } from './trading.service';
 import { TradingController } from './trading.controller';
-import { ConversionService } from './conversion.service';
-import { ConversionController } from './conversion.controller';
-import { TransferService } from './transfer.service';
-import { TransferController } from './transfer.controller';
 import { BinanceAdapter } from './adapters/binance.adapter';
+
 @Module({
   imports: [PrismaModule, LedgerModule],
-  controllers: [TradingController, ConversionController, TransferController],
-  providers: [TradingService, ConversionService, TransferService, BinanceAdapter],
-  exports: [TradingService, ConversionService, TransferService, BinanceAdapter],
+  controllers: [TradingController],
+  providers: [TradingService, BinanceAdapter],
+  exports: [TradingService, BinanceAdapter],
 })
 export class TradingModule {}
