cd004094ad8a1124b1b2a65488f627dde0863792
0williamsbernard0-sketch <0williamsbernard0@gmail.com>
2026-07-12 19:56:41 +0100

    Create create-withdrawal.dto.ts

---
+++ src/modules/withdrawals/dto/create-withdrawal.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsNotEmpty, Matches } from 'class-validator';
+
+export class CreateWithdrawalDto {
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[A-Z]{2,10}$/, { message: 'asset must be an uppercase asset code' })
+  asset: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^\d+(\.\d+)?$/, { message: 'amount must be a positive decimal string' })
+  amount: string;
+
+  @IsString()
+  @IsNotEmpty()
+  destinationAddress: string;
+}
