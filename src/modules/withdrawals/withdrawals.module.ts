6a80afb88fec26087337247340df6707c43ad292
0williamsbernard0-sketch <0williamsbernard0@gmail.com>
2026-07-14 14:42:41 +0100

    Update withdrawals.module.ts

--- src/modules/withdrawals/withdrawals.module.ts
+++ src/modules/withdrawals/withdrawals.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { LedgerModule } from '../ledger/ledger.module';
+import { WalletsModule } from '../wallets/wallets.module';
 import { WithdrawalsService } from './withdrawals.service';
 import { WithdrawalsController } from './withdrawals.controller';
 
 @Module({
-  imports: [PrismaModule, LedgerModule],
+  imports: [PrismaModule, LedgerModule, WalletsModule],
   controllers: [WithdrawalsController],
   providers: [WithdrawalsService],
 })
