7c06cb49f5119eac5183ae33bb8638b56fc83bc7
0williamsbernard0-sketch <0williamsbernard0@gmail.com>
2026-07-16 03:19:36 +0100

    Update subscriptions.module.ts

--- src/modules/subscriptions/subscriptions.module.ts
+++ src/modules/subscriptions/subscriptions.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { SubscriptionsService } from './subscriptions.service';
 import { SubscriptionsController } from './subscriptions.controller';
+import { AdminSubscriptionsController } from './admin-subscriptions.controller';
 import { PaystackService } from './paystack.service';
 import { NowPaymentsService } from './nowpayments.service';
 import { SubscriptionExpiryCron } from './subscription-expiry.cron';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [SubscriptionsController],
+  controllers: [SubscriptionsController, AdminSubscriptionsController],
   providers: [SubscriptionsService, PaystackService, NowPaymentsService, SubscriptionExpiryCron],
   exports: [SubscriptionsService],
 })
